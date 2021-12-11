c01c70c91fb4991e26b4864b35e60fd4b9885184
Peter Szollar <74240451+sollarp@users.noreply.github.com>
2021-12-11 11:00:47 +0000

    Update Main.kt
    Added comment to main.kt

--- treasurehunt/src/main/kotlin/Main.kt
+++ treasurehunt/src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
+/* Sort list from higher number to low. Add up all the numerical values of the list
+   and devided by Highets number (6). If total of devided number is'nt a whole number (Int) than keep     
+   adding number (4...) to highest number (6) until find a whole number which can be
+   diveded by total (16). If this gets the right value which could be devided with whole number than
+   that is the number of how many pirates can split the gold equally.
+*/
 
-val list = listOf(4,4,4).sorted().reversed()
+val list = listOf(6,3,4,2,1).sorted().reversed()
 
 fun getSum(list: List<Int>): Int {
     return list.sum()
